43f8127aca720d1082e2dc7c295f5563b643f1db
kuidreS <vitaliy.serdiuk@gmail.com>
2018-08-02 08:36:58 +0200

    Make the sheet active

---
+++ MakeSheetSelected/src/main/java/vserdiuk/apache/demo/model/Person.java
@@ -0,0 +1,93 @@
+package vserdiuk.apache.demo.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private Long phoneNumber;
+    private boolean married;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return married == person.married &&
+                Objects.equals(firstName, person.firstName) &&
+                Objects.equals(lastName, person.lastName) &&
+                Objects.equals(birthday, person.birthday) &&
+                Objects.equals(email, person.email) &&
+                Objects.equals(phoneNumber, person.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, birthday, email, phoneNumber, married);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthday=" + birthday +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", married=" + married +
+                '}';
+    }
+
+}
