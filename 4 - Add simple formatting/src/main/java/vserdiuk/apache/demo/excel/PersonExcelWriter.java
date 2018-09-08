15fd10059150a339ae1cf9dcedb8ac7a4fe8925d
kuidreS <vitaliy.serdiuk@gmail.com>
2018-09-08 16:15:10 +0200

    Adding a simple formatting to .xlsx file

--- 4 - Add simple formatting/src/main/java/vserdiuk/apache/demo/excel/PersonExcelWriter.java
+++ 4 - Add simple formatting/src/main/java/vserdiuk/apache/demo/excel/PersonExcelWriter.java
@@ -1,7 +1,6 @@
 package vserdiuk.apache.demo.excel;
 
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import vserdiuk.apache.demo.model.Person;
 
@@ -22,6 +21,12 @@ public class PersonExcelWriter {
         }
     }
 
+    /**
+     * The method prepares a Workbook by preparing the header row and the content area
+     *
+     * @param personList
+     * @return Workbook
+     */
     private Workbook prepareWorkbook(List<Person> personList) {
         Workbook workbook = new XSSFWorkbook();
         Sheet personSheet = workbook.createSheet("Persons");
@@ -32,6 +37,11 @@ public class PersonExcelWriter {
         return workbook;
     }
 
+    /**
+     * The method resizes columns according to a column content
+     *
+     * @param personSheet
+     */
     private void setAutoSizeColumn(Sheet personSheet) {
         int columnCount = personSheet.getRow(0).getLastCellNum()-1;
         for (int i=0; i<columnCount; i++) {
@@ -39,6 +49,11 @@ public class PersonExcelWriter {
         }
     }
 
+    /**
+     * The method prepares person sheet
+     *
+     * @param personSheet
+     */
     private void prepareHeader(Sheet personSheet) {
         //create row
         Row headerRow = personSheet.createRow(0);
@@ -71,6 +86,13 @@ public class PersonExcelWriter {
         marriedHeaderCell.setCellStyle(cellStyle);
     }
 
+    /**
+     * The method prepares person content area - lines with Person's data
+     * (First name, Last name, Birthday, Email, Phone number, Married)
+     *
+     * @param personSheet
+     * @param personList
+     */
     private void prapareTable(Sheet personSheet, List<Person> personList) {
         for (int i=0; i<personList.size(); i++) {
             //create row
@@ -105,6 +127,12 @@ public class PersonExcelWriter {
         }
     }
 
+    /**
+     * The method prepares the CellStyle for the header row
+     *
+     * @param personSheet
+     * @return
+     */
     private CellStyle getHeaderCellStyle(Sheet personSheet) {
         Font font = personSheet.getWorkbook().createFont();
         font.setBold(true); //setting font style as bold
@@ -125,6 +153,12 @@ public class PersonExcelWriter {
         return cellStyle;
     }
 
+    /**
+     * The method prepares the CellStyle for Person table content area
+     *
+     * @param personSheet
+     * @return
+     */
     private CellStyle getTableCellStyle(Sheet personSheet) {
         Font font = personSheet.getWorkbook().createFont();
         font.setItalic(true);
